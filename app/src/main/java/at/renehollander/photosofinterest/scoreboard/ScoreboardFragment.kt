291d75539ddea4d8dfdaaab265d00b1c985c82d0
Rene Hollander <mail@renehollander.at>
2018-05-08 14:13:03 +0200

    Initial design of scoreboard

--- app/src/main/java/at/renehollander/photosofinterest/scoreboard/ScoreboardFragment.kt
+++ app/src/main/java/at/renehollander/photosofinterest/scoreboard/ScoreboardFragment.kt
@@ -2,6 +2,9 @@ package at.renehollander.photosofinterest.scoreboard
 
 import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,14 +13,12 @@ import at.renehollander.photosofinterest.inject.scopes.ActivityScoped
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
+
 @ActivityScoped
 class ScoreboardFragment @Inject constructor() : DaggerFragment(), ScoreboardContract.View {
 
-    @Inject lateinit var presenter: ScoreboardContract.Presenter
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_scoreboard, container, false)
-    }
+    @Inject
+    lateinit var presenter: ScoreboardContract.Presenter
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -27,5 +28,29 @@ class ScoreboardFragment @Inject constructor() : DaggerFragment(), ScoreboardCon
     override fun onDestroy() {
         super.onDestroy()
         presenter.dropView()
+    }
+
+    private lateinit var dataset: Array<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initDataset()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_scoreboard, container, false)
+        val list = rootView.findViewById<RecyclerView>(R.id.scoreboard_list);
+        val layoutManager = LinearLayoutManager(activity)
+        list.layoutManager = layoutManager
+        list.adapter = ScoreboardAdapter(dataset)
+
+        val dividerItemDecoration = DividerItemDecoration(list.getContext(), layoutManager.getOrientation())
+        list.addItemDecoration(dividerItemDecoration)
+
+        return rootView
+    }
+
+    private fun initDataset() {
+        dataset = Array(3000, { i -> "User $i" })
     }
 }
