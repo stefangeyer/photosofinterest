11f36488ee55415687fd5b554c464588063f9873
Rene Hollander <mail@renehollander.at>
2018-05-08 21:59:21 +0200

    scoreboard now uses stupid mvp pattern

--- app/src/main/java/at/renehollander/photosofinterest/scoreboard/ScoreboardPresenter.kt
+++ app/src/main/java/at/renehollander/photosofinterest/scoreboard/ScoreboardPresenter.kt
@@ -1,11 +1,20 @@
 package at.renehollander.photosofinterest.scoreboard
 
 import at.renehollander.photosofinterest.UseCaseHandler
+import at.renehollander.photosofinterest.data.Image
+import at.renehollander.photosofinterest.data.ScoreboardEntry
+import at.renehollander.photosofinterest.data.User
 import javax.inject.Inject
 
 class ScoreboardPresenter @Inject constructor(
-        private val useCaseHandler: UseCaseHandler
 ) : ScoreboardContract.Presenter {
+    override fun fetchScores() {
+        this.view?.updateScores(mutableListOf(
+                ScoreboardEntry(null, User("user1@example.com", "User 1", Image("img1")), 20),
+                ScoreboardEntry(null, User("user2@example.com", "User 2", Image("img2")), 30),
+                ScoreboardEntry(null, User("user3@example.com", "User 3", Image("img2")), 100)
+        ))
+    }
 
     private var view: ScoreboardContract.View? = null
 
