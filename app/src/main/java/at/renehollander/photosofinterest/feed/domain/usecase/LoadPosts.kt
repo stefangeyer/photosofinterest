af45ce3e9864c1cb5a805c8792002212b860fa2f
Stefan Geyer <stefangeyer@outlook.com>
2018-05-09 02:10:42 +0200

    Implemented feed filter on UI and service layer

--- app/src/main/java/at/renehollander/photosofinterest/feed/domain/usecase/LoadPosts.kt
+++ app/src/main/java/at/renehollander/photosofinterest/feed/domain/usecase/LoadPosts.kt
@@ -3,6 +3,7 @@ package at.renehollander.photosofinterest.feed.domain.usecase
 import android.util.Log
 import at.renehollander.photosofinterest.UseCase
 import at.renehollander.photosofinterest.data.Post
+import at.renehollander.photosofinterest.data.source.LoadRecordCallback
 import at.renehollander.photosofinterest.data.source.PostDataSource
 import at.renehollander.photosofinterest.feed.domain.model.Filter
 import javax.inject.Inject
@@ -24,7 +25,7 @@ class LoadPosts @Inject constructor(
             Filter.TOP -> TODO()
         }
 
-        this.dataSource.loadPosts(object : PostDataSource.LoadRecordCallback<Post> {
+        this.dataSource.loadPosts(object : LoadRecordCallback<Post> {
             override fun onRecordsLoaded(records: List<Post>) {
                 this@LoadPosts.useCaseCallback?.onSuccess(ResponseValue(records))
                 Log.d(TAG, "Fetching posts was successful")
