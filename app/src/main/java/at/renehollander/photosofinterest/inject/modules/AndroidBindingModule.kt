192a89709f835f8e3c6669b428e35d41914135e5
Stefan Geyer <stefangeyer@outlook.com>
2018-05-09 12:00:06 +0200

    Bound ApplicationContext using a custom annotation

--- app/src/main/java/at/renehollander/photosofinterest/inject/modules/AndroidBindingModule.kt
+++ app/src/main/java/at/renehollander/photosofinterest/inject/modules/AndroidBindingModule.kt
@@ -1,11 +1,13 @@
 package at.renehollander.photosofinterest.inject.modules
 
 import android.app.Application
+import android.content.Context
 import at.renehollander.photosofinterest.PhotosOfInterest
 import at.renehollander.photosofinterest.auth.AuthActivity
 import at.renehollander.photosofinterest.auth.AuthModule
 import at.renehollander.photosofinterest.image.ImageActivity
 import at.renehollander.photosofinterest.image.ImageModule
+import at.renehollander.photosofinterest.inject.annotation.ApplicationContext
 import at.renehollander.photosofinterest.inject.scopes.ActivityScoped
 import at.renehollander.photosofinterest.inject.scopes.ApplicationScoped
 import at.renehollander.photosofinterest.main.MainActivity
@@ -17,11 +19,16 @@ import dagger.android.ContributesAndroidInjector
 @Module
 abstract class AndroidBindingModule {
 
-    // PhotosOfInterest -> Application
+    // PhotosOfInterest -> Application -> Context
 
     @Binds
     @ApplicationScoped
     abstract fun bindApplication(photosOfInterest: PhotosOfInterest): Application
+
+    @Binds
+    @ApplicationScoped
+    @ApplicationContext
+    abstract fun bindApplicationContext(application: Application): Context
 
     // activities
 
@@ -32,7 +39,6 @@ abstract class AndroidBindingModule {
     @ActivityScoped
     @ContributesAndroidInjector(modules = [ImageModule::class])
     abstract fun imageActivity(): ImageActivity
-
 
     @ActivityScoped
     @ContributesAndroidInjector(modules = [AuthModule::class])
