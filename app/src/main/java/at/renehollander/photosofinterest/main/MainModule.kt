2c0030bc31a8f5b6fe38dbfbd82a5611355104cd
Stefan Geyer <stefangeyer@outlook.com>
2018-05-09 12:50:33 +0200

    Reverted changes to MainModule: Cannot inject fragment manager from here is it was faulty

--- app/src/main/java/at/renehollander/photosofinterest/main/MainModule.kt
+++ app/src/main/java/at/renehollander/photosofinterest/main/MainModule.kt
@@ -1,39 +1,25 @@
 package at.renehollander.photosofinterest.main
 
-import android.content.Context
-import android.support.v4.app.FragmentManager
 import at.renehollander.photosofinterest.challenges.ChallengesModule
 import at.renehollander.photosofinterest.feed.FeedModule
-import at.renehollander.photosofinterest.inject.annotation.ApplicationContext
 import at.renehollander.photosofinterest.inject.scopes.ActivityScoped
 import at.renehollander.photosofinterest.profile.ProfileModule
 import at.renehollander.photosofinterest.scoreboard.ScoreboardModule
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 
-
 @Module(includes = [
-    MainModule.Declarations::class,
     ChallengesModule::class,
     FeedModule::class,
     ProfileModule::class,
     ScoreboardModule::class
 ])
 abstract class MainModule {
+    @ContributesAndroidInjector
+    abstract fun mainActivity(): MainActivity
 
-    @Module
-    interface Declarations {
-        @ContributesAndroidInjector
-        fun mainActivity(): MainActivity
-
-        @Binds
-        @ActivityScoped
-        fun mainPresenter(presenter: MainPresenter): MainContract.Presenter
-    }
-
-    @Provides
+    @Binds
     @ActivityScoped
-    fun fragmentManager(activity: MainActivity): FragmentManager = activity.supportFragmentManager
+    abstract fun mainPresenter(presenter: MainPresenter): MainContract.Presenter
 }
