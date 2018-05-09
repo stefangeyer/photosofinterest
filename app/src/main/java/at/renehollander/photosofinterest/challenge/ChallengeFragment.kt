c722831d4d8ab6088dfe6511e716f7d4d036cc43
Rene Hollander <mail@renehollander.at>
2018-05-09 13:07:10 +0200

    challenge details now have a working viewpager

--- app/src/main/java/at/renehollander/photosofinterest/challenge/ChallengeFragment.kt
+++ app/src/main/java/at/renehollander/photosofinterest/challenge/ChallengeFragment.kt
@@ -5,8 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import at.renehollander.photosofinterest.R
+import at.renehollander.photosofinterest.challenge.details.ChallengeDetailsFragment
 import at.renehollander.photosofinterest.data.Post
-import dagger.Lazy
+import at.renehollander.photosofinterest.feed.FeedFragment
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_challenge.*
 import javax.inject.Inject
@@ -15,9 +16,12 @@ class ChallengeFragment @Inject constructor() : DaggerFragment(), ChallengeContr
 
     @Inject
     lateinit var presenter: ChallengeContract.Presenter
-
     @Inject
-    lateinit var adapter: Lazy<ChallengeFragmentPagerAdapter>
+    lateinit var challengeDetailsFragment: ChallengeDetailsFragment
+    @Inject
+    lateinit var feedFragment: FeedFragment
+
+    lateinit var adapter: ChallengeFragmentPagerAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_challenge, container, false)
@@ -25,8 +29,12 @@ class ChallengeFragment @Inject constructor() : DaggerFragment(), ChallengeContr
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-        viewPager.adapter = adapter.get()
+        adapter = ChallengeFragmentPagerAdapter(childFragmentManager);
+        adapter.context = context!!
+        adapter.challengeDetailsFragment = challengeDetailsFragment
+        adapter.feedFragment = feedFragment
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
     }
 
     override fun onResume() {
