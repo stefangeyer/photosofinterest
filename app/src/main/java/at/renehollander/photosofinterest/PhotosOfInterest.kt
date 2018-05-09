aba292122257e5c0e8475d5d394519d3ffa4c8e5
Rene Hollander <mail@renehollander.at>
2018-05-09 18:33:19 +0200

    sign in now sets fake user in application

--- app/src/main/java/at/renehollander/photosofinterest/PhotosOfInterest.kt
+++ app/src/main/java/at/renehollander/photosofinterest/PhotosOfInterest.kt
@@ -1,5 +1,7 @@
 package at.renehollander.photosofinterest
 
+import at.renehollander.photosofinterest.data.Image
+import at.renehollander.photosofinterest.data.User
 import at.renehollander.photosofinterest.inject.DaggerApplicationComponent
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.jakewharton.threetenabp.AndroidThreeTen
@@ -8,10 +10,32 @@ import dagger.android.support.DaggerApplication
 
 class PhotosOfInterest : DaggerApplication() {
 
+    private var user: User? = null
+
     override fun onCreate() {
         super.onCreate()
         Fresco.initialize(this)
         AndroidThreeTen.init(this)
+    }
+
+    fun login() {
+        user = User(
+                "user@example.com",
+                "Max Mustermann",
+                Image("https://pbs.twimg.com/profile_images/547371527855828992/qSbjhsoI_400x400.jpeg")
+        )
+    }
+
+    fun isLoggedIn(): Boolean {
+        return user != null
+    }
+
+    fun logout() {
+        user = null
+    }
+
+    fun getUser(): User {
+        return user!!
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication>? =
