dc2b0cf622aeba8c6a750b97f8007438c88eb639
Stefan Geyer <stefangeyer@outlook.com>
2018-06-12 23:07:25 +0200

    Af few graphical changes to the challenge UI

--- app/src/main/java/at/renehollander/photosofinterest/challenge/ChallengeContract.kt
+++ app/src/main/java/at/renehollander/photosofinterest/challenge/ChallengeContract.kt
@@ -5,6 +5,7 @@ import at.renehollander.photosofinterest.BasePresenter
 import at.renehollander.photosofinterest.BaseView
 import at.renehollander.photosofinterest.challenge.details.ChallengeDetailsContract
 import at.renehollander.photosofinterest.data.Challenge
+import at.renehollander.photosofinterest.data.Post
 
 /**
  * Contract for the feed parentView
@@ -16,6 +17,8 @@ interface ChallengeContract {
     interface View : BaseView {
         fun startPhotoTake()
         fun getDetailsPresenter(): ChallengeDetailsContract.Presenter
+        fun updateChallengePosts(posts: List<Post>)
+        fun showCannotReload()
         fun startLogin()
     }
 
@@ -24,6 +27,7 @@ interface ChallengeContract {
         fun photoTaken(photo: Bitmap)
 
         fun setChallenge(challenge: Challenge?)
+        fun loadChallengePosts()
         fun update()
     }
 }
--- app/src/main/java/at/renehollander/photosofinterest/challenge/ChallengePresenter.kt
+++ app/src/main/java/at/renehollander/photosofinterest/challenge/ChallengePresenter.kt
@@ -2,11 +2,16 @@ package at.renehollander.photosofinterest.challenge
 
 import android.graphics.Bitmap
 import at.renehollander.photosofinterest.PhotosOfInterest
+import at.renehollander.photosofinterest.UseCaseHandler
 import at.renehollander.photosofinterest.data.Challenge
+import at.renehollander.photosofinterest.feed.domain.model.RequestFilter
+import at.renehollander.photosofinterest.feed.domain.usecase.LoadPosts
 import javax.inject.Inject
 
 class ChallengePresenter @Inject constructor(
-        private val application: PhotosOfInterest
+        private val application: PhotosOfInterest,
+        private val useCaseHandler: UseCaseHandler,
+        private val loadPosts: LoadPosts
 ) : ChallengeContract.Presenter {
 
     private var view: ChallengeContract.View? = null
@@ -35,6 +40,17 @@ class ChallengePresenter @Inject constructor(
     override fun setChallenge(challenge: Challenge?) {
         this.challenge = challenge
         this.view?.getDetailsPresenter()?.setChallenge(challenge)
+    }
+
+    override fun loadChallengePosts() {
+        this.useCaseHandler.execute(this.loadPosts, LoadPosts.RequestValues(RequestFilter.TOP), {
+            // Success
+            response ->
+            this.view?.updateChallengePosts(response.posts)
+        }, {
+            // Failure
+            this.view?.showCannotReload()
+        })
     }
 
     override fun update() {
