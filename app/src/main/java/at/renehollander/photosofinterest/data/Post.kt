20609f71ced436b31b26c3df2805842fd8b891cc
Rene Hollander <mail@renehollander.at>
2018-06-13 13:16:33 +0200

    Added add post functionality.

--- app/src/main/java/at/renehollander/photosofinterest/data/Post.kt
+++ app/src/main/java/at/renehollander/photosofinterest/data/Post.kt
@@ -2,5 +2,12 @@ package at.renehollander.photosofinterest.data
 
 import com.google.firebase.firestore.GeoPoint
 
-data class Post(var id: String = "", var user: User, var challenge: Challenge, var title: String, var image: Image,
-                var upvotes: Int, var downvotes: Int, var origin: GeoPoint, var poi: PointOfInterest)
+data class Post(var id: String = "",
+                var user: User,
+                var challenge: Challenge,
+                var title: String,
+                var image: String,
+                var upvotes: Int,
+                var downvotes: Int,
+                var origin: GeoPoint,
+                var poi: PointOfInterest)
--- app/src/main/java/at/renehollander/photosofinterest/data/source/PostDataSource.kt
+++ app/src/main/java/at/renehollander/photosofinterest/data/source/PostDataSource.kt
@@ -1,6 +1,9 @@
 package at.renehollander.photosofinterest.data.source
 
+import at.renehollander.photosofinterest.data.Challenge
+import at.renehollander.photosofinterest.data.PointOfInterest
 import at.renehollander.photosofinterest.data.Post
+import com.google.firebase.firestore.GeoPoint
 
 /**
  * Example data source that provides operations for a sample entity
@@ -14,4 +17,6 @@ interface PostDataSource {
     enum class Filter {
         RISING, RECENT, TOP
     }
+
+    fun addPost(challenge: Challenge, poi: PointOfInterest, title: String, image: String, origin: GeoPoint, callback: GetRecordCallback<Post>)
 }
