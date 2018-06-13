265aaa7a1bd6d9198ebcd8b7c4f834b058c44c43
Rene Hollander <mail@renehollander.at>
2018-06-13 23:49:52 +0200

    filter challenges

--- app/src/main/java/at/renehollander/photosofinterest/data/source/ChallengeDataRepository.kt
+++ app/src/main/java/at/renehollander/photosofinterest/data/source/ChallengeDataRepository.kt
@@ -9,6 +9,7 @@ import at.renehollander.photosofinterest.timestampToLocalDateTime
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
+import org.threeten.bp.LocalDateTime
 import javax.inject.Inject
 
 /**
@@ -42,40 +43,19 @@ class ChallengeDataRepository @Inject constructor(
                 }
                 return@map challenge
             }.toList()
-            callback.onRecordsLoaded(challenges)
+            callback.onRecordsLoaded(when (filter) {
+                ChallengeDataSource.Filter.ALL -> challenges
+                ChallengeDataSource.Filter.NEARBY -> challenges.filter {
+                    it.end.isAfter(LocalDateTime.now())
+                }
+                ChallengeDataSource.Filter.ONGOING -> challenges.filter {
+                    it.end.isAfter(LocalDateTime.now())
+                }
+            })
             Log.d("ChallengeDataRepository", challenges.toString())
         }.addOnFailureListener {
             callback.onDataNotAvailable()
         }
-
-//        val challenge1 = Challenge(
-//                title = "Flüsse oder so",
-//                image = Image("http://ferienstar.de/wp-content/uploads/2017/02/sieghart-reisen-woerthersee.jpg"),
-//                start = LocalDateTime.now().minusDays(1), end = LocalDateTime.now().plusDays(1),
-//                description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-//                regions = mutableListOf(Region("Carinthia", mutableListOf()), Region("Lower Austria", mutableListOf())), pois = mutableListOf())
-//        val challenge2 = Challenge(
-//                title = "Kärntner Seen",
-//                image = Image("https://webheimat.at/aktiv/Urlaub/Tipps/Woerthersee-Sommer-Events/Woerthersee-Sommer_high.jpg"),
-//                start = LocalDateTime.now().minusDays(1), end = LocalDateTime.now().plusDays(4).plusHours(2),
-//                description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-//                regions = mutableListOf(Region("Carinthia", mutableListOf())), pois = mutableListOf())
-//
-//        val challenge3 = Challenge(
-//                title = "Wiener Denkmäler",
-//                image = Image("https://www.wien.info/media/images/altstadt-panorama-mit-stephansdom-und-karlskirche-19to1.jpeg"),
-//                start = LocalDateTime.now().minusDays(4), end = LocalDateTime.now().plusDays(7).plusHours(2),
-//                description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-//                regions = mutableListOf(Region("Wien oida", mutableListOf())), pois = mutableListOf())
-//
-//
-//        val out = when (filter) {
-//            NEARBY -> listOf(challenge3, challenge2)
-//            ONGOING -> listOf(challenge1, challenge3)
-//            ALL -> listOf(challenge1, challenge2, challenge3)
-//        }
-//
-//        callback.onRecordsLoaded(out)
     }
 
     override fun loadChallengeDetails(challenge: Challenge, callback: GetRecordCallback<Challenge>) {
