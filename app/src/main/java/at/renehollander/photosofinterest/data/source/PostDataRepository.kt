74fc3ac4726fe24ff3340c3007a446a8782c868c
Rene Hollander <mail@renehollander.at>
2018-06-13 17:02:38 +0200

    profile now show users posts.

--- app/src/main/java/at/renehollander/photosofinterest/data/source/PostDataRepository.kt
+++ app/src/main/java/at/renehollander/photosofinterest/data/source/PostDataRepository.kt
@@ -70,6 +70,68 @@ class PostDataRepository @Inject constructor(
         callback.onRecordsLoaded(mutableListOf())
     }
 
+    override fun loadPosts(user: User, callback: LoadRecordCallback<Post>) {
+        db.collection("challenges").get().continueWithTask { challengesData ->
+            return@continueWithTask Tasks.whenAllSuccess<List<Post>>(challengesData.result.documents.map { challengeData ->
+                @Suppress("UNCHECKED_CAST")
+                val challenge = Challenge()
+                challenge.id = challengeData.id
+                challenge.title = challengeData["title"] as String
+                challenge.image = challengeData["image"] as String
+                challenge.start = timestampToLocalDateTime(challengeData["start"] as Timestamp)
+                challenge.end = timestampToLocalDateTime(challengeData["end"] as Timestamp)
+                challenge.description = challengeData["description"] as String
+                challenge.regions = (challengeData["regions"] as List<Map<String, Any?>>).map mapi@{
+                    val region = Region()
+                    region.description = it["description"] as String
+                    region.region = it["region"] as List<GeoPoint>
+                    return@mapi region
+                }
+
+                return@map challengeData.reference.collection("posts").get().continueWithTask inner@{
+                    return@inner Tasks.whenAllSuccess<Post>(it.result.documents.filter { (it["user"] as DocumentReference).id.equals(user.id) }.map postDataMap@{ postData ->
+                        val post = Post(
+                                id = postData.id,
+                                user = User(),
+                                challenge = challenge,
+                                title = postData["title"] as String,
+                                image = postData["image"] as String,
+                                upvotes = (postData["upvotes"] as Long).toInt(),
+                                downvotes = (postData["downvotes"] as Long).toInt(),
+                                origin = postData["origin"] as GeoPoint,
+                                timestamp = postData["timestamp"] as Timestamp,
+                                poi = PointOfInterest()
+                        )
+                        return@postDataMap (postData["user"] as DocumentReference).get().continueWith {
+                            post.user = User(
+                                    id = it.result.id,
+                                    email = it.result["email"] as String,
+                                    name = it.result["name"] as String,
+                                    image = it.result["image"] as String
+                            )
+                            return@continueWith post
+                        }.continueWithTask pointOfInterestMap@{
+                            return@pointOfInterestMap (postData["poi"] as DocumentReference).get().continueWith {
+                                post.poi = PointOfInterest(
+                                        id = it.result.id,
+                                        name = it.result["name"] as String? ?: "",
+                                        location = it.result["location"] as GeoPoint?
+                                                ?: GeoPoint(0.0, 0.0)
+                                )
+                                return@continueWith post
+                            }
+                        }
+                    })
+                }
+            })
+        }.addOnSuccessListener {
+            callback.onRecordsLoaded(it.flatMap { it.toList() })
+        }.addOnFailureListener {
+            Log.e(TAG, "Error fetching posts", it)
+            callback.onDataNotAvailable()
+        }
+    }
+
     override fun addPost(challenge: Challenge, poi: PointOfInterest, title: String, image: String, origin: GeoPoint, callback: GetRecordCallback<Post>) {
         val timestamp = Timestamp.now()
         val data = mutableMapOf<String, Any>()
--- app/src/main/java/at/renehollander/photosofinterest/data/source/PostDataSource.kt
+++ app/src/main/java/at/renehollander/photosofinterest/data/source/PostDataSource.kt
@@ -3,6 +3,7 @@ package at.renehollander.photosofinterest.data.source
 import at.renehollander.photosofinterest.data.Challenge
 import at.renehollander.photosofinterest.data.PointOfInterest
 import at.renehollander.photosofinterest.data.Post
+import at.renehollander.photosofinterest.data.User
 import com.google.firebase.firestore.GeoPoint
 
 /**
@@ -20,4 +21,5 @@ interface PostDataSource {
 
     fun addPost(challenge: Challenge, poi: PointOfInterest, title: String, image: String, origin: GeoPoint, callback: GetRecordCallback<Post>)
     fun loadPosts(challenge: Challenge, callback: LoadRecordCallback<Post>)
+    fun loadPosts(user: User, callback: LoadRecordCallback<Post>)
 }
