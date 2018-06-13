9c9387f78549386b8f6e4afd639e43bdedc92df8
Stefan Geyer <stefangeyer@outlook.com>
2018-06-13 23:04:59 +0200

    Added Parcelable implementations of the models

--- app/src/main/java/at/renehollander/photosofinterest/data/Challenge.kt
+++ app/src/main/java/at/renehollander/photosofinterest/data/Challenge.kt
@@ -1,5 +1,7 @@
 package at.renehollander.photosofinterest.data
 
+import android.os.Parcel
+import android.os.Parcelable
 import org.threeten.bp.LocalDateTime
 
 data class Challenge(var id: String = "",
@@ -9,4 +11,39 @@ data class Challenge(var id: String = "",
                      var end: LocalDateTime = LocalDateTime.of(1970, 1, 1, 0, 0, 0),
                      var description: String = "",
                      var regions: List<Region> = mutableListOf(),
-                     var pois: List<PointOfInterest> = mutableListOf())
+                     var pois: List<PointOfInterest> = mutableListOf()) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readSerializable() as LocalDateTime,
+            parcel.readSerializable() as LocalDateTime,
+            parcel.readString(),
+            parcel.createTypedArrayList(Region.CREATOR),
+            parcel.createTypedArrayList(PointOfInterest))
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(title)
+        parcel.writeString(image)
+        parcel.writeSerializable(start)
+        parcel.writeSerializable(end)
+        parcel.writeString(description)
+        parcel.writeTypedList(regions)
+        parcel.writeTypedList(pois)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Challenge> {
+        override fun createFromParcel(parcel: Parcel): Challenge {
+            return Challenge(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Challenge?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
